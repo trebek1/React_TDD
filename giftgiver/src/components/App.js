f69c9333f23e590080af602d86b029d323696584
Alex Trzeciak <trebek08@gmail.com>
2017-12-02 13:22:55 -0800

    hooked up app to server

--- giftgiver/src/components/App.js
+++ giftgiver/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'; 
+import { Button } from 'react-bootstrap'; 
 
 // Want App to have react methods for component delegated to App class
 class App extends Component {
@@ -7,10 +8,24 @@ class App extends Component {
 		super();
 		this.state = {gifts: []};
 	}
+
+	// with => the same this object is passed to fn 
+	addGift = () => {
+		const { gifts } = this.state;
+		const ids = this.state.gifts.map(gift => gift.id);
+
+		const maxId = ids.length > 0 ? Math.max(...ids) : 0; 
+
+		gifts.push({id: maxId + 1});
+		
+		this.setState({gifts}); 
+	}	
+
 	render(){
 		return (
 			<div>
 				<h2> Gift Giver </h2>
+				<Button onClick={this.addGift} className="btn-add"> Add Gift</Button>
 			</div>
 		)
 	}
