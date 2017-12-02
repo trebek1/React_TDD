0b5793efdebf57cd43ee75a7787cd4045975114b
Alex Trzeciak <trebek08@gmail.com>
2017-12-02 12:49:19 -0800

    Created and passed test to create app

---
+++ giftgiver/src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'; 
+
+// Want App to have react methods for component delegated to App class
+class App extends Component {
+	render(){
+		return (
+			<div></div>
+		)
+	}
+}
+
+export default App; 
--- giftgiver/src/components/App.test.js
+++ giftgiver/src/components/App.test.js
@@ -4,10 +4,13 @@ import React from 'react';
 // only render outer jsx but not inner content (shallow)
 import { shallow } from 'enzyme'; 
 
-import App from '/.App'; 
+import Adapter from 'enzyme-adapter-react-16';
+
+import App from './App'; 
 
 const app = shallow(<App />);
 
 it("renders correctly", () => {
+	// Snapshot keeps a history of each change to component that is made 
 	expect(app).toMatchSnapshot();
 });
