80a027422db8185460d244431bb1c1186674336d
Alex Trzeciak <trebek08@gmail.com>
2017-12-02 13:55:26 -0800

    updated tests for BDD and eliminated test pollution

--- giftgiver/src/components/App.test.js
+++ giftgiver/src/components/App.test.js
@@ -6,25 +6,42 @@ import { shallow } from 'enzyme';
 
 import App from './App'; 
 
-const app = shallow(<App />);
+describe("App", () => {
+	const app = shallow(<App />);
 
-it("renders correctly", () => {
-	// Snapshot keeps a history of each change to component that is made 
-	expect(app).toMatchSnapshot();
-});
+	it("renders correctly", () => {
+		// Snapshot keeps a history of each change to component that is made 
+		expect(app).toMatchSnapshot();
+	});
 
-it("initializes the `state` with an empty list of gifts", ()=> {
-	expect(app.state().gifts).toEqual([]); 
-});
+	it("initializes the `state` with an empty list of gifts", ()=> {
+		expect(app.state().gifts).toEqual([]); 
+	});
 
-it("adds a new `state` when clicking the `add gift` button", () =>{
-	// find child nodes with jsx tags or className
-	app.find('.btn-add').simulate('click'); 
-	expect(app.state().gifts).toEqual([{id: 1}]); 
-});
+	describe('when clicking the `add-gift` button', () => {
+		beforeEach(() => {
+			app.find('.btn-add').simulate('click'); 
+		});
 
-it("adds a new gift to the rendered list when clicking the `add gift` button", () => {
-	// test pollution
-	app.find('.btn-add').simulate('click');
-	expect(app.find('.gift-list').children().length).toEqual(2);
-});
+		afterEach(() => {
+			app.setState({gifts: []});
+		}); 
+
+		it("adds a new `state`", () =>{
+			// find child nodes with jsx tags or className
+			expect(app.state().gifts).toEqual([{id: 1}]); 
+		});
+
+		it("adds a new gift to the rendered list", () => {
+			// test pollution
+			expect(app.find('.gift-list').children().length).toEqual(1);
+		});
+	}); 
+});	
+
+
+
+
+// BDD given, when, then to get around test pollution 
+
+// group test together by type 
