6a6bb7c2d309490c6de70f1e2b28b116687c1c91
Alex Trzeciak <trebek08@gmail.com>
2017-12-02 12:58:50 -0800

    set an initial state to pass teh test with an empty array

--- giftgiver/src/components/App.js
+++ giftgiver/src/components/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 // Want App to have react methods for component delegated to App class
 class App extends Component {
+	// super will call constructor of parent component class
+	constructor(){
+		super();
+		this.state = {gifts: []};
+	}
 	render(){
 		return (
 			<div>
