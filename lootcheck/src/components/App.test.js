d5f5d46dcd3f6d819c976b236e25ae4a4be7fc5b
Alex Trzeciak <trebek08@gmail.com>
2017-12-06 19:23:30 -0800

    added styling

--- lootcheck/src/components/App.test.js
+++ lootcheck/src/components/App.test.js
@@ -19,4 +19,8 @@ describe('App', () =>{
   it("contains a connected loot component ", () => {
     expect(app.find("Connect(Loot)").exists()).toBe(true);
   });
+
+  it("contains a link to the coindesk price page ", () => {
+    expect(app.find('a').props().href).toBe('https://www.coindesk.com/price');
+  }); 
 }); 
